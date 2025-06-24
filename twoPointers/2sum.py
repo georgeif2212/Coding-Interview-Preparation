eb09ea3e28a3669b45791bf3a10b71e7b99705e3
Jorge Infante <jinfante2212@gmail.com>
2025-06-23 20:52:50 -0500

    feat: new version using sets and array unsorted

--- twoPointers/2sum.py
+++ twoPointers/2sum.py
@@ -24,3 +24,29 @@ print(x)  # Expected output: [[0, 3], [1, 2]]
 #   i                j   -2,   2
 #       i            j   -1,   2
 #              i     j    2,   2        [3,5]=2
+
+
+def two_sum_array_not_sorted(nums, target):
+    result = []
+    seen = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            print("SEEN[DIFF]", seen[diff])
+            result.append([seen[diff], i])
+        seen[num] = i
+    print(seen)
+    return result
+
+
+
+# [-1, 0, 1, 2, -1, -4]
+#   0  1  2  3   4   5
+x = two_sum_array_not_sorted([-1, -2, 1, 2, -1, -4, 5], 3)
+print(x)
+
+
+# Crear un diccionario
+frecuencia = {'a': 2, 'b': 1}
+frecuencia['c'] = 3
+print(frecuencia['c'])
