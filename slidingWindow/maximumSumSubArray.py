d7dc7584521905e0eedbb7004bda92527c20ca4b
Jorge Infante <jinfante2212@gmail.com>
2025-06-23 21:59:41 -0500

    feat: new version without using sets

--- slidingWindow/maximumSumSubArray.py
+++ slidingWindow/maximumSumSubArray.py
@@ -35,4 +35,31 @@ def maximumSubarraySum(nums, k):
 arr = [2, 1, 5, 1, 3, 2]
 k = 3
 max_sum = maximumSubarraySum(arr, k)
-print(max_sum)
+# print(max_sum)
+
+
+def max_sum_subarray_k(v, k):
+    # Suma inicial de los primeros k elementos
+    sum_ = sum(v[:k])
+    max_sum = sum_
+    res_i = 0
+    res_j = k - 1
+
+    # 
+    for j in range(k, len(v)):
+        sum_ += v[j] - v[j - k]
+        # print(sum_, j, ":", v[j], "j-k:",j-k, v[j - k])
+        if sum_ > max_sum:
+            max_sum = sum_
+            res_i = j - k + 1
+            res_j = j
+
+    return v[res_i : res_j + 1], max_sum, res_i, res_j
+
+
+arr = [2- k, 3, 2, 5, 1, 9]
+print(max_sum_subarray_k(arr, 1))
+# [2, 3, 2, 5, 1, 5]
+# [i-----j]
+# [2, 3, 2, 5, 1, 5]
+#    [i-----j]
