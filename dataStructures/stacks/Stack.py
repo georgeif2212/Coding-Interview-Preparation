02a1dc6eabb123445f526d79e9e2429244eb6e35
Jorge Infante <jinfante2212@gmail.com>
2025-06-25 17:27:05 -0500

    feat: stack implemented#

--- dataStructures/stacks/Stack.py
+++ dataStructures/stacks/Stack.py
@@ -1,15 +1,29 @@
 class Stack:
     def __init__(self):
-        self.data=[]
+        self.data = []
 
-    def push(self,x):
+    def push(self, x):
         self.data.append(x)
-    
-    def pop(self,x):
+        return f"Pushed {x}"
+
+    def pop(self):
         return self.data.pop()
 
-    def top(self,x):
+    def top(self):
         return self.data[-1]
-    
+
     def empty(self):
         return not self.data
+
+    def __str__(self):
+        return f"Pila (base → cima): {self.data}"
+
+
+s = Stack()
+print(s.empty())
+print(s.push(2))
+print(s.top())
+print(s.push(9))
+print(s.push(3))
+print(s.empty())
+print(s)
