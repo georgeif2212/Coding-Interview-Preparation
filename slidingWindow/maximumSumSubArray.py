bb189adcbe96588783982642c08a49cdfedee471
Jorge Infante <jinfante2212@gmail.com>
2025-06-25 15:09:26 -0500

    feat: new version of chatgpt

--- slidingWindow/maximumSumSubArray.py
+++ slidingWindow/maximumSumSubArray.py
@@ -45,7 +45,7 @@ def max_sum_subarray_k(v, k):
     res_i = 0
     res_j = k - 1
 
-    # 
+    #
     for j in range(k, len(v)):
         sum_ += v[j] - v[j - k]
         # print(sum_, j, ":", v[j], "j-k:",j-k, v[j - k])
@@ -57,8 +57,17 @@ def max_sum_subarray_k(v, k):
     return v[res_i : res_j + 1], max_sum, res_i, res_j
 
 
-arr = [2- k, 3, 2, 5, 1, 9]
+def max_sum_subarray_chat(arr, k):
+    max_sum = curr_sum = sum(arr[:k])
+    for i in range(k, len(arr)):
+        curr_sum += arr[i] - arr[i - k]
+        max_sum = max(max_sum, curr_sum)
+    return max_sum
+
+
+arr = [2 - k, 3, 2, 5, 1, 9]
 print(max_sum_subarray_k(arr, 1))
+print(max_sum_subarray_chat(arr, 3))
 # [2, 3, 2, 5, 1, 5]
 # [i-----j]
 # [2, 3, 2, 5, 1, 5]
