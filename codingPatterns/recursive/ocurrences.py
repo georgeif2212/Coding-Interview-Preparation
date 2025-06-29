91ab1979b2f15bacb65d26d0f1d0dc01e04ec8b0
Jorge Infante <jinfante2212@gmail.com>
2025-06-28 22:21:26 -0500

    feat: recursive programs

--- codingPatterns/recursive/ocurrences.py
+++ codingPatterns/recursive/ocurrences.py
@@ -8,4 +8,12 @@ def count_occur(nums, num):
     return count + count_occur(nums[1:], num)
 
 
+def count_occur(nums, num, i=0):
+    if i == len(nums):
+        return 0
+
+    count = 1 if nums[i] == num else 0
+    return count + count_occur(nums, num, i + 1)
+
+
 print(count_occur([1, 2, 3, 2, 2, 5], 2))
