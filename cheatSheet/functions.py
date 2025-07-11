73f36413834754efc32b3db2828522072b373872
Jorge Infante <jinfante2212@gmail.com>
2025-07-10 22:42:15 -0500

    feat: cheatsheet which includes most common methods, functions, etc

---
+++ cheatSheet/functions.py
@@ -0,0 +1,7 @@
+len(arr)                   # longitud de la lista
+sum(arr)                   # suma de los elementos
+max(arr)                   # mayor elemento
+min(arr)                   # menor elemento
+sorted(arr)                # nueva lista ordenada (ascendente)
+sorted(arr, reverse=True)  # orden descendente
+list(reversed(arr))        # reversa como lista nueva
