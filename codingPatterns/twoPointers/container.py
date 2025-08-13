ddc13a20e241d6ef02fb6a2f1abd1d32d427e3f7
Jorge Infante <jinfante2212@gmail.com>
2025-08-12 20:03:56 -0500

    feat: new version

--- codingPatterns/twoPointers/container.py
+++ codingPatterns/twoPointers/container.py
@@ -14,6 +14,19 @@ def containerWithMostWater(height):
     return maximum
 
 
+def biggestContainer(height):
+    left = 0
+    right = len(height) - 1
+    maximum = 0
+    while left < right:
+        maximum = max(maximum, (right - left) * min(height[left], height[right]))
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+        return maximum
+
+
 height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 maximum = containerWithMostWater(height)
 print(maximum)
