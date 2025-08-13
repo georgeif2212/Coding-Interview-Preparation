348fee0ee22e575cc22274e84dbe6fa39fa25583
Jorge Infante <jinfante2212@gmail.com>
2025-08-12 20:48:58 -0500

    feat: program which finds the intersection between two lists

---
+++ codingPatterns/twoPointers/getIntersectionNode.py
@@ -0,0 +1,11 @@
+def getIntersectionNode(headA, headB):
+    if not headA or not headB:
+        return None
+
+    a, b = headA, headB
+
+    while a != b:
+        a = a.next if a else headB
+        b = b.next if b else headA
+
+    return a
